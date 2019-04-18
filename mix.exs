55f4d861b05cb0d8dfd2e809a55b790ccae5d7d1
Andrew Forward <aforward@gmail.com>
2019-04-18 09:46:03 -0400

    Add doex and version_tasks for deployment

--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule Techblog.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:doex, "~> 0.9"},
+      {:version_tasks, "~> 0.11"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
