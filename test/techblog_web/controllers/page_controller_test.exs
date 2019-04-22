20de7ffe9c117628425fc0cd538c2030289e907c
Andrew Forward <aforward@gmail.com>
2019-04-22 19:07:01 -0400

    Just look for 'v' on the main page

--- test/techblog_web/controllers/page_controller_test.exs
+++ test/techblog_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TechblogWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "techblog"
+    assert html_response(conn, 200) =~ "v"
   end
 end
