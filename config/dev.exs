587bd95ecb7b786a18a7edcca1da21c43e19e10f
Andrew Forward <aforward@gmail.com>
2019-05-14 12:18:14 -0400

    Live reload when you edit a markdown (.md) file

--- config/dev.exs
+++ config/dev.exs
@@ -59,6 +59,7 @@ config :techblog, TechblogWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
+      ~r"assets/blog/.*(md)$",
       ~r"lib/techblog_web/{live,views}/.*(ex)$",
       ~r"lib/techblog_web/templates/.*(eex)$"
     ]
--- lib/techblog_web/controllers/page_controller.ex
+++ lib/techblog_web/controllers/page_controller.ex
@@ -2,28 +2,47 @@ defmodule TechblogWeb.PageController do
   use TechblogWeb, :controller
 
   @slugs Techblog.slugs()
-
   @articles Techblog.articles()
-
   @summaries Techblog.summaries()
 
   def index(conn, _params) do
     conn
-    |> assign(:slugs, @slugs)
-    |> assign(:summaries, @summaries)
+    |> assign(:slugs, slugs())
+    |> assign(:summaries, summaries())
     |> render("index.html")
   end
 
   def show(conn, %{"slug" => slug}) do
-    case @slugs[slug] do
+    case slugs()[slug] do
       nil ->
         redirect(conn, to: "/")
 
       _details ->
         conn
         |> assign(:slug, slug)
-        |> assign(:article, @articles[slug])
+        |> assign(:article, articles()[slug])
         |> render("show.html")
+    end
+  end
+
+  def slugs() do
+    case Mix.env() do
+      :dev -> Techblog.slugs()
+      _ -> @slugs
+    end
+  end
+
+  def articles() do
+    case Mix.env() do
+      :dev -> Techblog.articles()
+      _ -> @articles
+    end
+  end
+
+  def summaries() do
+    case Mix.env() do
+      :dev -> Techblog.summaries()
+      _ -> @summaries
     end
   end
 end
