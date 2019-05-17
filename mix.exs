1615f32152d80b3f584abe25abf94d58ce86ddd0
Andrew Forward <aforward@gmail.com>
2019-05-17 07:17:28 -0400

    v0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Techblog.MixProject do
   def project do
     [
       app: :techblog,
-      version: "0.2.7",
+      version: "0.3.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
