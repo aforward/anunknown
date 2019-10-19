05ad61cea305cf47f84579fe7c2f7063aeb07e35
Andrew Forward <aforward@gmail.com>
2019-10-19 14:24:26 -0400

    New space added

--- test/techblog_test.exs
+++ test/techblog_test.exs
@@ -23,7 +23,7 @@ defmodule TechblogTest do
     assert %{
              "setting-up-blog" => %{
                html:
-                 "<h1>Setting Up Your Blog</h1>\n<h2>April 20, 2019</h2>\n<p>This is your full-length article.</p>\n<p>To display images that will display in github and\non your website, follow this pattern.</p>\n<p><img src=\"/images/artboard.png\" alt=\"Art\"/></p>\n",
+                 "<h1>Setting Up Your Blog</h1>\n<h2>April 20, 2019</h2>\n<p>This is your full-length article.</p>\n<p>To display images that will display in github and\non your website, follow this pattern.</p>\n<p><img src=\"/images/artboard.png\" alt=\"Art\" /></p>\n",
                sort: "20190420",
                tags: ["elixir", "erlang"],
                title: "Setting Up Your Blog"
