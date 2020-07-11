91421b31faa621da7d6353e2ab336013c735e48c
Andrew Forward <aforward@gmail.com>
2020-07-11 15:04:57 -0400

    Fix test and update deps

--- test/techblog_test.exs
+++ test/techblog_test.exs
@@ -23,7 +23,7 @@ defmodule TechblogTest do
     assert %{
              "setting-up-blog" => %{
                html:
-                 "<h1>Setting Up Your Blog</h1>\n<h2>April 20, 2019</h2>\n<p>This is your full-length article.</p>\n<p>To display images that will display in github and\non your website, follow this pattern.</p>\n<p><img src=\"/images/artboard.png\" alt=\"Art\" /></p>\n",
+                 "<h1>\nSetting Up Your Blog</h1>\n<h2>\nApril 20, 2019</h2>\n<p>\nThis is your full-length article.</p>\n<p>\nTo display images that will display in github and\non your website, follow this pattern.</p>\n<p>\n  <img src=\"/images/artboard.png\" alt=\"Art\" />\n</p>\n",
                sort: "20190420",
                tags: ["elixir", "erlang"],
                title: "Setting Up Your Blog"
@@ -35,7 +35,7 @@ defmodule TechblogTest do
     assert %{
              "setting-up-blog" => %{
                html:
-                 "<h1>Setting Up Your Blog</h1>\n<h2>April 20, 2019</h2>\n<p>This is your summary.</p>\n",
+                 "<h1>\nSetting Up Your Blog</h1>\n<h2>\nApril 20, 2019</h2>\n<p>\nThis is your summary.</p>\n",
                sort: "20190420",
                tags: ["elixir", "erlang"],
                title: "Setting Up Your Blog"
